e9e5d1c6cfa9e0ee11bbeacc29ee7df8ef72ffb1
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2024-08-02 09:09:53 +0545

    Starting of C sharp Programs

---
+++ Program1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DiceRollRandomExample{
+public class Program{
+    public static void Main(string[] args){
+        Random dice = new Random();
+        int roll = dice.Next(0, 10);
+        Console.WriteLine($"Roll is: {roll}");
+    }
+}
+}
