da895c3625283fc89786846fe49bdd4c83be462d
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2024-08-02 12:26:06 +0545

    Program 2 and Program 3 added that contains simple Math function

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace DiceRollRandomExampleOvrload{
+    public class Program{
+        public static void Main(string[] args){
+            Random dice  = new Random();
+            int roll1 = dice.Next();
+            int roll2 = dice.Next(1,10);
+          int roll3 = dice.Next(20);
+          Console.WriteLine($"roll1 is {roll1} \t roll2 is {roll2} \t roll3 is {roll3}");    
+        }
+    }
+}
---
+++ Program3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GreaterUsingStaticMathExample{
+    public class GreaterUsingStaticMathExample{
+        public static void Main(string[] args){
+            int FirstNo = 600;
+            int SecondNo = 500;
+            int GreaterNo = Math.Max(FirstNo,SecondNo);
+            Console.WriteLine(GreaterNo);
+        }
+    }
+}
